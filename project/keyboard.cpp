85390d9113f165b7844b42810128dda602a55ed7
kezhengjie <kzj961014@qq.com>
2022-05-21 16:08:15 +0800

    fix warning

--- project/keyboard.cpp
+++ project/keyboard.cpp
@@ -1,7 +1,7 @@
 #include "keyboard.h"
 
-KeyboardHandler::Handler KeyboardHandler::handler_ = [](KeyCode keyCode,
-                                                        KeyState keyStatus) -> void {};
+KeyboardHandler::Handler KeyboardHandler::handler_ = [](KeyCode,
+                                                        KeyState) -> void {};
 
 HHOOK KeyboardHandler::hook_ = NULL;
 
